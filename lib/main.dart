0a84fa046b456da187580a9bce11d362635a804e
Akshatjain2003 <akshatjainofficial20@gmail.com>
2023-08-23 19:43:35 +0530

    theme: make purples lighter

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:expense_app/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 96, 59, 181),
+  seedColor: Color.fromARGB(255, 104, 62, 202),
 );
 
 var kDarkColorScheme = ColorScheme.fromSeed(
